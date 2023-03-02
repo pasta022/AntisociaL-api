c65c42112a8437b3eaffad50edb254eaf175ca7d
Anu Olajide <102215210+pasta022@users.noreply.github.com>
2023-03-02 18:00:16 +0100

    feat: changed the port

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
 
-app.listen(3000, () => {
+app.listen(8000, () => {
     console.log('hello papa');
 })
