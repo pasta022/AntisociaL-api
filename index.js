64e2f22e3a09ea7fca6d261aa455b65e33a44d97
Anu Olajide <102215210+pasta022@users.noreply.github.com>
2023-06-16 23:55:03 +0100

    feat: installed multer to use as local storage for posts with pictures

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const { default: mongoose } = require("mongoose");
 const dotenv = require("dotenv");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
 
 const app = express();
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        const fileName = Date.now()+ file.originalname
+        cb(null, fileName);
+    }
+});
+
+const upload = multer({storage});
 
 dotenv.config();
 
@@ -17,14 +31,33 @@ mongoose.connect(process.env.MONGO_URL, () => {
 });
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+  next();
+});
+
 
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
+app.use('/images', express.static(path.join(__dirname, "/public/images")));
+
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+        res.json({
+            fileName: req.file.filename
+        })
+        return res.status(200).json("File Uploaded successfully");
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+})
 
 app.listen(8000, () => {
     console.log('hello papa');
-})
+});
--- routes/users.js
+++ routes/users.js
@@ -104,6 +104,26 @@ router.put('/:id/unfollow', async (req, res) => {
     }
 })
 
+//get following
+router.get("/following/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const friends = await Promise.all(
+            user.following.map(friendId => {
+                return User.findById(friendId);
+            })
+        );
+        let friendList = [];
+        friends.map(friend => {
+            const { _id, username, profilePicture } = friend;
+            friendList.push({ _id, username, profilePicture });
+        });
+        res.status(200).json(friendList);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
 
 
 module.exports = router;
