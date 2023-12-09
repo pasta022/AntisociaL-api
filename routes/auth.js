33f8824fdc05f00c2b9652134c8e2fb1da5c7f03
Anu Olajide <102215210+pasta022@users.noreply.github.com>
2023-12-09 21:28:34 +0100

    return block for auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,53 +1,47 @@
-const router = require('express').Router();
-const User = require('../models/user')
-const bcrypt = require('bcrypt');
-
+const router = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 //register user
-router.post('/register', async (req, res) => {
-    
-    try {
-        //generate password 
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+router.post("/register", async (req, res) => {
+  try {
+    //generate password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-        //create user
-        const newUser = await new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword
-        });
+    //create user
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
 
-        //save user and return resposne 
-        const user = await newUser.save();
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json(error);
-    }
+    //save user and return resposne
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-
-
-//login user 
+//login user
 router.post("/login", async (req, res) => {
-    try {
+  try {
+    //find user
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).send("User not Found.\n Sign Up!!! ");
 
-        //find user
-        const user = await User.findOne({ email: req.body.email });
-        !user && res.status(404).send("User not Found.\n Sign Up!!! ");
+    //check password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).send("wrong password");
 
-        //check password
-        const validPassword = await bcrypt.compare(req.body.password, user.password);
-        !validPassword && res.status(400).send('wrong password');
-
-        res.status(200).json(user);
-
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
-
-
-
+    res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 
 module.exports = router;
