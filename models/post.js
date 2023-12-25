c3a4ed811b9883de02affef9119f7c018328f52c
Anu Olajide <102215210+pasta022@users.noreply.github.com>
2023-12-25 14:32:16 +0100

    post images uploaded to cloudinary

--- models/post.js
+++ models/post.js
@@ -12,7 +12,7 @@ const postSchema = new mongoose.Schema({
     img: {
         type: String,
     },
-    likes: {
+    likes: { 
         type: Array,
         default: []
     },
--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,17 @@
 const router = require("express").Router();
 const Post = require("../models/post");
 const User = require('../models/user');
+// const cloudinary = require("../cloudinary")
 
 //create post
 router.post("/", async (req, res) => {
-    const newPost = await new Post(req.body);
     try {
+        const newPost = await new Post(req.body);
         const savedPost = await newPost.save();
         res.status(200).json(savedPost);
     } catch (error) {
         res.status(500).json(error);
-    }
+    } 
 });
 
 
